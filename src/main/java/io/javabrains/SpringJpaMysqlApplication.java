f5d89807d7a048c99ebfca354c417b02eb2f6a5b
Latha <Latha@DESKTOP-GP0TL91>
2020-05-13 00:19:43 +0100

    Initial commit

--- src/main/java/io/javabrains/SpringJpaMysqlApplication.java
+++ src/main/java/io/javabrains/SpringJpaMysqlApplication.java
@@ -2,14 +2,21 @@ package io.javabrains;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "io.javabrains")
-public class SpringJpaMysqlApplication {
+public class SpringJpaMysqlApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJpaMysqlApplication.class, args);
 	}
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringJpaMysqlApplication.class); 
+	} 
+
 
 }
